4869d73f9d5ee3a37674dae141c4aaeb853e98dc
onlyexchange <ordersonlyexchange@gmail.com>
2025-08-18 16:17:55 +0800

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=()" }
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
